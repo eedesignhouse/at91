0ef96f821414fedf584f37f3f3f9b9266d67b062
liyong <liyong521@gmail.com>
2018-04-15 15:57:44 +0800

    initial

---
+++ image/at91sam9x5ek_demo_linux_nandflash_lcd043_JLINK.bat
@@ -0,0 +1,3 @@
+"D:\Program Files (x86)\ATMEL Corporation\sam-ba_2.11\sam-ba.exe" \jlink\ARM0 AT91SAM9X35-EK at91sam9x5ek_demo_linux_nandflash_lcd043.tcl > logfile.log 2>&1
+notepad logfile.log
+
---
+++ image/at91sam9x5ek_demo_linux_nandflash_lcd043_USB.bat
@@ -0,0 +1,3 @@
+sam-ba.exe \usb\ARM0 AT91SAM9X35-EK at91sam9x5ek_demo_linux_nandflash_lcd043.tcl > logfile.log 2>&1
+notepad logfile.log
+
